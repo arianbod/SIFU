c34dc3853da777a5295f1e82b26e509c7e1bef80
Arian <Arianbod@outlook.com>
2023-07-29 19:17:06 +0300

    Post Page added

--- KromRegal/src/components/structure/body/Blog/PostItem.js
+++ KromRegal/src/components/structure/body/Blog/PostItem.js
@@ -3,30 +3,30 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import Badge from '@mui/material/Badge';
+import { Link } from "react-router-dom";
 import { CardActionArea } from '@mui/material';
 import Photo from "../../../../assets/LoremIpsum.webp"
 export default function ActionAreaCard(props) {
     return (
-
-        <Card>
-            <CardActionArea>
-                <CardMedia
-                    component="img"
-                    height="250"
-                    image={Photo}
-                    alt={props.Title}
-                />
-                <CardContent>
-                    <Typography gutterBottom variant="h5" component="div">
-                        {props.Title}
-                    </Typography>
-                    <Typography variant="body2" color="text.secondary">
-                        {props.PostText}
-                    </Typography>
-                </CardContent>
-            </CardActionArea>
-        </Card>
-
+        <Link key={props.PostId} to={`/Blog/${props.PostId}/${props.PostSlug}`} underline="none">
+            <Card>
+                <CardActionArea>
+                    <CardMedia
+                        component="img"
+                        height="250"
+                        image={Photo}
+                        alt={props.Title}
+                    />
+                    <CardContent>
+                        <Typography gutterBottom variant="h5" component="div">
+                            {props.PostTitle}
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            {props.PostText}
+                        </Typography>
+                    </CardContent>
+                </CardActionArea>
+            </Card>
+        </Link >
     );
 }
--- KromRegal/src/components/structure/body/Blog/PostPage.js
+++ KromRegal/src/components/structure/body/Blog/PostPage.js
@@ -1,46 +1,111 @@
-// // PostPage.js
-// import React, { useEffect, useState } from "react";
-// import { useParams } from "react-router-dom";
-// import { fetchData } from "./dataFetcher"; // Import the data fetching function
+import React from "react";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
+import Container from "@mui/material/Container";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Navigation from "../../general/Navigation";
+import Footer from '../../general/8_Footer';
+import PostItem from './PostItem';
+import FeaturedPost from './FeaturedPost';
+import Search from "./Search";
+import { Link, useParams } from "react-router-dom";
+import PostsData from './Data.json';
+const darkTheme = createTheme({
+    typography: {
+        fontFamily: "Poppins, sans-serif",
+    },
+    palette: {
+        mode: "dark",
+        primary: {
+            main: "#CE1620",
+        },
+        background: {
+            default: "#14141A",
+        },
+    },
+});
 
-// function PostPage() {
-//     const { id } = useParams(); // Get the post ID from the URL parameter
-//     const [loading, setLoading] = useState(true);
-//     const [post, setPost] = useState(null);
+function Cundoctor() {
+    const theme = useTheme();
+    const isMdScreen = useMediaQuery(theme.breakpoints.up("md"));
+    const isxlScreen = useMediaQuery(theme.breakpoints.up("xl"));
+    const { id, Slug } = useParams();
+    const PostData = PostsData.PostsData.find(
+        (post) => post.id.toString() === id
+    );
+    console.log("hello" + PostData.Title)
+    return (
+        <>
+            <ThemeProvider theme={darkTheme}>
 
-//     useEffect(() => {
-//         fetchData() // Fetch data from the API or JSON
-//             .then((data) => {
-//                 // Find the post with the matching ID
-//                 const foundPost = data.find((item) => item.id === Number(id));
-//                 if (foundPost) {
-//                     setPost(foundPost);
-//                 } else {
-//                     console.error("Post not found");
-//                 }
-//                 setLoading(false);
-//             })
-//             .catch((error) => {
-//                 console.error("Error fetching data:", error);
-//                 setLoading(false);
-//             });
-//     }, [id]);
+                <CssBaseline />
+                {/* <Container maxWidth="xl"> */}
+                <header className="App-header mb">
+                    <Navigation />
+                </header>
+                <Box id="body">
+                    <Grid
+                        container
+                        sx={{
+                            backgroundSize: "cover",
+                            display: "flex",
+                            justifyContent: "center",
+                            backgroundRepeat: "no-repeat",
+                            backgroundPosition: "center ",
+                            width: "100%",
+                            alignItems: "top",
 
-//     return (
-//         <div>
-//             {loading ? (
-//                 <p>Loading...</p>
-//             ) : post ? (
-//                 <div>
-//                     <h1>{post.Title}</h1>
-//                     <p>{post.PostText}</p>
-//                     {/* Additional post content can be displayed here */}
-//                 </div>
-//             ) : (
-//                 <p>Post not found.</p>
-//             )}
-//         </div>
-//     );
-// }
+                        }}
+                    >
+                        <Grid item xs={12}>
+                            <Grid
+                                container
+                                sx={{
+                                    backgroundSize: "cover",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundPosition: "center ",
+                                    width: "100%",
+                                    alignItems: "top",
+                                }}
+                            >
+                                <Search />
+                            </Grid>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <FeaturedPost Title={PostData.Title} />
+                        </Grid>
 
-// export default PostPage;
+                        <Grid item xs={12}>
+
+
+
+                            <Grid
+                                container
+                                sx={{
+                                    backgroundSize: "cover",
+                                    display: "flex",
+                                    justifyContent: "center",
+                                    backgroundRepeat: "no-repeat",
+                                    backgroundPosition: "center ",
+                                    width: "100%",
+                                    alignItems: "top",
+                                    mt: 4,
+                                }}
+                            >
+
+                            </Grid>
+                        </Grid>
+                    </Grid>
+                </Box>
+                <Footer />
+            </ThemeProvider >
+        </>
+    );
+}
+
+export default Cundoctor;
